a0608b569685fb8031484c15b3d939386ab58af8
Costin Botez <costibotez94@gmail.com>
2017-07-21 00:53:37 +0100

    Create admin page for rendering API Key

--- wp-content/plugins/wp-flg360/admin/class-wp-flg360-admin.php
+++ wp-content/plugins/wp-flg360/admin/class-wp-flg360-admin.php
@@ -41,6 +41,24 @@ class Wp_Flg360_Admin {
 	private $version;
 
 	/**
+	 * The hook suffix of Admin Setting page
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_screen_hook_suffix
+	 */
+	private $plugin_screen_hook_suffix;
+
+	/**
+	 * The options name to be used in this plugin
+	 *
+	 * @since  	1.0.0
+	 * @access 	private
+	 * @var  	string 		$option_name 	Option name of this plugin
+	 */
+	private $option_name = 'wp_flg360_api';
+
+	/**
 	 * Initialize the class and set its properties.
 	 *
 	 * @since    1.0.0
@@ -98,6 +116,76 @@ class Wp_Flg360_Admin {
 
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-flg360-admin.js', array( 'jquery' ), $this->version, false );
 
+	}
+
+	/**
+	 * Add an options page under the Settings submenu
+	 *
+	 * @since  1.0.0
+	 */
+	public function add_options_page() {
+
+		$this->plugin_screen_hook_suffix = add_options_page(
+			__( 'FLG360 Integration', 'wp-flg360' ),
+			__( 'FLG360 Integration', 'wp-flg360' ),
+			'manage_options',
+			$this->plugin_name,
+			array( $this, 'display_options_page' )
+		);
+	}
+
+	/**
+	 * Render the options page for plugin
+	 *
+	 * @since  1.0.0
+	 */
+	public function display_options_page() {
+		include_once 'partials/wp-flg360-admin-display.php';
+	}
+
+	/**
+	 * Register setting section for plugin
+	 *
+	 * @since  1.0.0
+	 */
+	public function register_setting() {
+		// Add a General section
+		add_settings_section(
+			$this->option_name . '_general',
+			__( 'General Settings', 'wp-flg360' ),
+			array( $this, $this->option_name . '_general_cb' ),
+			$this->plugin_name
+		);
+
+		add_settings_field(
+			$this->option_name . '_key',
+			__( 'API Key', 'wp-flg360' ),
+			array( $this, $this->option_name . '_key_cb' ),
+			$this->plugin_name,
+			$this->option_name . '_general',
+			array( 'label_for' => $this->option_name . '_position' )
+		);
+
+		register_setting( $this->plugin_name, $this->option_name . '_key' );
+	}
+
+	/**
+	 * Render the text for the general section
+	 *
+	 * @since  1.0.0
+	 */
+	public function wp_flg360_api_general_cb() {
+		echo '<p>' . __( 'Please change the settings accordingly.', 'wp-flg360' ) . '</p>';
+	}
+
+	/**
+	 * Render the API Key input for this plugin
+	 *
+	 * @since  1.0.0
+	 */
+	public function wp_flg360_api_key_cb() {
+		$api_key = get_option( $this->option_name . '_key' );
+		echo '<input type="text" name="' . $this->option_name . '_key' . '" id="' . $this->option_name . '_key' . '" value="' . $api_key . '"/>';
 	}
 
 }
